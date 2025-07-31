9030a0464323f4c5d855e2db143c822f3ef4ea60
lakshmigarments <nkthangaisanju@gmail.com>
2025-07-31 14:15:32 +0530

    last

--- src/chat-bot.jsx
+++ src/chat-bot.jsx
@@ -247,12 +247,12 @@ Make it visually appealing and emoji-rich.
   };
 
   return (
-    <div className="container-fluid p-0 vh-100">
+    <div className="container-fluid p-0">
       <div className="card vh-100 bg-transparent border-0 shadow-sm">
         <div className="position-sticky bg-black card">
           <div className="row">
             <div className="col-2  toggle d-flex justify-content-center align-items-center mt-4">
-              <div><button className="btn border" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-toggles" viewBox="0 0 16 16">
+              <div><button className="btn  color-border" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-toggles" viewBox="0 0 16 16">
   <path d="M4.5 9a3.5 3.5 0 1 0 0 7h7a3.5 3.5 0 1 0 0-7zm7 6a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m-7-14a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5m2.45 0A3.5 3.5 0 0 1 8 3.5 3.5 3.5 0 0 1 6.95 6h4.55a2.5 2.5 0 0 0 0-5zM4.5 0h7a3.5 3.5 0 1 1 0 7h-7a3.5 3.5 0 1 1 0-7"/>
 </svg></button></div>
               
